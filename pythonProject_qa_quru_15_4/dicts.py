cc5a9974cadcce1b404347ce9dfeccd61cb5d555
Sergei Gorobets <sergei.gorobets@vk.team>
2024-07-27 17:02:25 +0300

    lesson 4

---
+++ pythonProject_qa_quru_15_4/dicts.py
@@ -0,0 +1,17 @@
+# ЗАВОДИМ СЛОВАРИ (Они пишутся в фигурных скобках)
+
+d = {
+    'key': "value",
+    "another": 'another value'
+}
+
+user1 = {
+    'name': "Sergei",
+    'age': 27
+}
+
+user2 = {
+    'name': "Vasya",
+    'age': 25
+}
+
